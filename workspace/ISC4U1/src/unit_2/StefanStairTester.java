896282cb7ae6a2c10378339e6d2c366b16da1681
Mike Stefan <mikegstefan66@gmail.com>
2021-11-17 23:11:00 -0500

    done ig

--- workspace/ISC4U1/src/unit_2/StefanStairTester.java
+++ workspace/ISC4U1/src/unit_2/StefanStairTester.java
@@ -2,18 +2,44 @@ package unit_2;
 
 import becker.robots.*;
 import java.util.Random;
+/**
+ * Tests the stair sweeper robot
+ * @author Mike
+ *
+ */
 public class StefanStairTester {
+	/**
+	 * main testing function
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		// vairables used to configure the stair construction  (these should be low enough that summed they will still fit on the screen)
+		//bottom level of the stairs
+		int floorSt = 7;
+		//how far from the robots starting position the first stair should be
+		int stairStart = 2;
+		//how many steps up are wanted
+		int stairUpLen = 2; 
+		//how long is the top platform of the stairs
+		int stairTopLen = 2;
+		//how many steps down are wanted
+		int stairDownLen = 4;
+		//how far from the last step should the garbage be
+		int garbageDist = 2;
 		
-		int floorSt = 9, stairStart = 2, stairUpLen = 4, stairTopLen = 2, stairDownLen = 4, garbageDist = 3;
+		//set up objects
 		City oakville = new City(10,15);
 		StefanSweeperRobot sweep = new StefanSweeperRobot(oakville,floorSt,0,Direction.EAST); 
-				
+		
+		
+		//generate the floor from the robots starting position to the first step
 		Wall[] floor1 = new Wall[stairStart];
 		for(int i = 0; i< stairStart; i++) {
 			floor1[i] = new Wall(oakville, floorSt,i,Direction.SOUTH);
 		}
 		
+		
+		//generate the stairs going up
 		Wall[][] stairUp = new Wall[stairUpLen][2];
 		for(int i = 0; i <stairUpLen; i++) {
 			stairUp[i][1] = new Wall(oakville, i+floorSt-stairUpLen+1,stairStart+ stairUpLen-i-1,Direction.WEST);
@@ -21,45 +47,70 @@ public class StefanStairTester {
 		}
 		
 		
+		//generate the top level of the stairs
 		Wall[] stairTop = new Wall[stairTopLen];
 		for(int i = 0; i<stairTopLen; i++) {
 			stairTop[i]= new Wall(oakville, floorSt-stairUpLen+1,stairStart+ stairUpLen+i-1,Direction.NORTH);
 		}
 		
 		
+		//generate the stairs going down
 		Wall[][] stairDown = new Wall[stairDownLen][2];
 		for(int i = 0; i <stairDownLen; i++) {
 			stairDown[i][1] = new Wall(oakville, i+floorSt-stairUpLen+1,i+stairStart+stairUpLen+stairTopLen-2,Direction.EAST);
 			stairDown[i][0] = new Wall(oakville, i+floorSt-stairUpLen+1,i+stairStart+stairUpLen+stairTopLen-2,Direction.NORTH);
 		}
 		
-		Wall[] floor2 = new Wall[garbageDist];
-		for(int i = 0; i < garbageDist; i++) {
+		//generate the floor conneting the bottom stair of the down section to the garbage
+		Wall[] floor2 = new Wall[garbageDist+2];
+		for(int i = 0; i < garbageDist+2; i++) {
 			floor2[i] = new Wall(oakville, floorSt-stairUpLen+stairDownLen, i+stairStart+stairUpLen+stairTopLen+stairDownLen-2,Direction.SOUTH);
 		}
 		
 		
+		//generate the garbage can
 		Wall[] garbage = new Wall[3];
 		garbage[0] = new Wall(oakville,floorSt-stairUpLen+stairDownLen,stairStart+stairUpLen+stairTopLen+stairDownLen+garbageDist-2,Direction.WEST);
 		garbage[1] = new Wall(oakville,floorSt-stairUpLen+stairDownLen,stairStart+stairUpLen+stairTopLen+stairDownLen+garbageDist-2,Direction.SOUTH);
 		garbage[2] = new Wall(oakville,floorSt-stairUpLen+stairDownLen,stairStart+stairUpLen+stairTopLen+stairDownLen+garbageDist-2,Direction.EAST);
 		
+		
+		//randomly generate the things on valid positions
 		Thing[] things = new Thing[stairStart+stairUpLen+stairTopLen+stairDownLen+garbageDist-2];
 		Random rand = new Random();
 		for(int i = 0; i <stairStart+stairUpLen+stairTopLen+stairDownLen+garbageDist-2; i++) {
-//			if(rand.nextInt(2) == 1) {
+			// robot will have a 1/2 chance of spawning a thing for every valid position
+			if(rand.nextInt(2) == 1) {
+				//calulates the proper street for the thing given the avenue 
 				int y = floorSt;
-				if (i <stairStart)y = floorSt;
-				else if(i >stairStart-1 && i <stairStart+stairUpLen) y -= i-stairStart+1;
-				else if(i <stairStart+stairUpLen+stairTopLen -1)y = floorSt-stairUpLen;
-				else if(i < stairStart+stairUpLen+stairTopLen+stairDownLen-1)y = floorSt-stairUpLen+i-(stairStart+stairUpLen+stairTopLen)+2;
-				else y = floorSt-stairUpLen+stairDownLen;
+				if (i <stairStart) {
+					//if ave is before the stairs
+					y = floorSt;
+				}
+				else if(i >stairStart-1 && i <stairStart+stairUpLen) {
+					//if ave is during the climb up
+					y -= i-stairStart+1;
+				}
+				else if(i <stairStart+stairUpLen+stairTopLen -1) {
+					//if ave is along the top of the stairs
+					y = floorSt-stairUpLen;
+				}
+				else if(i < stairStart+stairUpLen+stairTopLen+stairDownLen-1) {
+					//if ave is during the climb down
+					y = floorSt-stairUpLen+i-(stairStart+stairUpLen+stairTopLen)+2;
+				}
+				else {
+					//if ave is between the bottom of the stairs and the garbage
+					y = floorSt-stairUpLen+stairDownLen;
+				}
+				
+				//adds the thing
 				things[i] = new Thing(oakville, y,i);
-//			}
+			}
 			
 		}
 
-		
+		//send robot to clean
 		sweep.sweepStairs();
 	}
 }
--- workspace/ISC4U1/src/unit_2/StefanSweeperRobot.java
+++ workspace/ISC4U1/src/unit_2/StefanSweeperRobot.java
@@ -5,9 +5,7 @@ import becker.robots.*;
  * a special version of the becker RobotSE designed to clean a simulated stair case
  * @author Mike
  * 
- *@pre: The robot is on the ground facing a set of stairs and a garbage can is after the
-stairs. You can assume each step is always one by one and that the garbage can is a height and
-width of one
+ *@pre: the stairs are constructed as highlighted in our design document
  */
 public class StefanSweeperRobot extends RobotSE{
 	/**
@@ -44,36 +42,48 @@ public class StefanSweeperRobot extends RobotSE{
 	}
 	
 	/**
-	 * Robot takes a single step down and sets up for the next step
-	 * @return if the step was not possible, only happens when the garbage is 1 square away from the bottom of the stairs
+	 * trys to pick up a thing and Robot takes a single step down and sets up for the next step
 	 * @pre the robot is above an empty space(no wall) and facing south
-	 * @post
+	 * @post one square over and one down, facing south OR if the robot could not move to the next ave due to hitting the garbage, just one block down facing south
 	 */
-	private boolean stepDown() {
+	private void stepDown() {
 		this.move();
 		this.safePickThing();
 		this.turnLeft();
-		if(!this.frontIsClear())return true;
-		this.move();
+		if(this.frontIsClear())this.move();
 		this.turnRight();
-		return false;
 	}
-	
+	/**
+	 * gets the robot from its starting position to the bottom of the stairs, picking up all possible things along the way
+	 * @pre robot is facing east (the stairs)
+	 * @post robot is one square to the right of the stairs facing east
+	 */
 	private void moveStair() {
+		//has the robot reached the stairs
 		while(this.frontIsClear()) {
 			this.safePickThing();
 			this.move();
 		}
 	}
+	/**
+	 * moves along the top platform of the stairs until it can move down, picking up all possible things along the way
+	 * @pre the robot is on top of the stairs, facing east
+	 * @post the robot is one ave past the top of the stairs, facing down
+	 */
 	
 	private void findDown() {
+		//try to find the stairs
 		while(true) {
 			this.safePickThing();
 			this.turnRight();
+			//is there no wall below the robot (found the end of the top of the stairs)
 			if(this.frontIsClear()) {
+				//stop looking
 				break;
 			}
+			
 			else {
+				//continue moving
 				this.turnLeft();
 				this.safePickThing();
 				this.move();
@@ -81,31 +91,49 @@ public class StefanSweeperRobot extends RobotSE{
 		}
 
 	}
-	
+	/**
+	 * Climbs up the stairs
+	 * @pre robot is facing east, against a wall
+	 * @post robot is at the top of the stairs, facing east
+	 */
 	private void climbUp() {
+		//step up until there is nolonger a stair infront
 		while(!this.frontIsClear()) {
 			this.stepUp();
 		}
 	}
-	
+	/**
+	 * robot climbs down the stairs
+	 * @pre robot is 1 ave past the end of the top of the stairs, facing down
+	 * @post robot is at the bottom of the stairs, facing east
+	 */
 	private void climbDown() {
+		//take a step down until there is floor below
 		while(this.frontIsClear()) {
-			if(this.stepDown()) {
-				return;
-			}
+			this.stepDown();
 		}
+		//face ending direction
 		this.turnLeft();
 		
 	}
-	
+	/**
+	 * moves from the end of the stairs to the ave 1 to the left of the garbage, picking up all possible things along the way
+	 * @pre robot is on the floor, on the other side of the stairs facing east
+	 * @post robot is one block to the left of the garbage, facing it (east)
+	 */
 	private void findGarbage() {
+		//move until the robot hits the garbage
 		while(this.frontIsClear()) {
 			this.safePickThing();
 			this.move();
 		}
 	}
-	
-	private void moveToGarbage() {
+	/**
+	 * Moves the robot from beside the garbage into it
+	 * @pre robot is one ave to the left of the garbage, facing east
+	 * @post robot is in the garbage facing south
+	 */
+	private void moveIntoGarbage() {
 		this.safePickThing();
 		this.turnLeft();
 		this.move();
@@ -114,16 +142,45 @@ public class StefanSweeperRobot extends RobotSE{
 		this.turnRight();
 		this.move();
 	}
-	
+	/**
+	 * robot empties all things into the garbage
+	 * @pre robot is in the garbage
+	 * @post robot is in the garbage, with all things put down
+	 */
 	private void throwGarbage() {
 		this.putAllThings();
 	}
 	
-	private void dumpGarbage() {
-		this.moveToGarbage();
-		this.throwGarbage();
+	/**
+	 * robot leaves garbage and parks to the right of it
+	 * @pre robot is in the garbage, facing south
+	 * @post robot is one ave right of the garbage, facing east
+	 */
+	private void leaveGarbage() {
+		this.turnAround();
+		this.move();
+		this.turnRight();
+		this.move();
+		this.turnRight();
+		this.move(); 
+		this.turnLeft();
 	}
 	
+	/**
+	 * robot moves beside the garbage, into it and puts all things
+	 * @pre robot is one ave to the left of the garbage, facing east
+	 * @post robot is one ave right of the garbage, facing east
+	 */
+	private void dumpGarbage() {
+		this.moveIntoGarbage();
+		this.throwGarbage();
+		this.leaveGarbage();
+	}
+	/**
+	 * cleans the stairs
+	 * @pre robot is facing east and there are properly built stairs
+	 * @post stairs are cleaned with the robot being 1 ave right of the garbage facing east
+	 */
 	public void sweepStairs() {
 		this.moveStair();
 		this.climbUp();
